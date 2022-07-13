7ef075335bced0d05f1f79fd3ff34c7dc240775b
Selmentausen <selmentausen@gmail.com>
2022-07-13 13:59:23 +0300

    Added posts showcase

--- bwetter/bwetter/urls.py
+++ bwetter/bwetter/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('post/', include('posts.urls')),
     path('profile/', include('user_profile.urls')),
+    path('', index)
 ]
--- bwetter/posts/views.py
+++ bwetter/posts/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def dashboard(request):
-    return render(request, 'base.html')
+    posts = []
+    for profile in request.user.profile.follows.all():
+        for post in profile.user.posts.all():
+            posts.append(post)
+    posts = sorted(posts, key=lambda s: s.created_date, reverse=True)
+    return render(request, 'post_list.html', {'posts': posts})
